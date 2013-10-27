3e6879c217b44a58c78302c851a644e9cda3afc5
Sergey Zenchenko <sergeizenchenko@gmail.com>
2013-10-27 20:21:54 +0200

    Add event bus to base activity;

--- src/main/java/com/zen/droidparts/ui/fragment/BaseActivity.java
+++ src/main/java/com/zen/droidparts/ui/fragment/BaseActivity.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import butterknife.Views;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by zen on 10/27/13.
  */
 public abstract class BaseActivity extends FragmentActivity {
 
+    private EventBus eventBus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setEventBus(new EventBus());
 
         int contentResourse = getContentViewResource();
         if (contentResourse > 0) {
@@ -24,7 +29,27 @@ public abstract class BaseActivity extends FragmentActivity {
         afterCreateView(savedInstanceState);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getEventBus().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getEventBus().unregister(this);
+    }
+
     protected abstract int getContentViewResource();
 
     protected abstract void afterCreateView(Bundle savedInstanceState);
+
+    public EventBus getEventBus() {
+        return eventBus;
+    }
+
+    public void setEventBus(EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
 }
