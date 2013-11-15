3d54f26f5cb29644a3eb7b6672a68b264219970a
Sergey Zenchenko <sergeizenchenko@gmail.com>
2013-11-15 15:35:09 +0200

    Remove retrofit module and add networking config class;

---
+++ src/main/java/com/zen/droidparts/networking/NetworkingModuleConfig.java
@@ -0,0 +1,16 @@
+package com.zen.droidparts.networking;
+
+/**
+ * Created by zen on 11/15/13.
+ */
+public class NetworkingModuleConfig {
+    private final String serverAddress;
+
+    public NetworkingModuleConfig(String serverAddress) {
+        this.serverAddress = serverAddress;
+    }
+
+    public String getServerAddress() {
+        return serverAddress;
+    }
+}
