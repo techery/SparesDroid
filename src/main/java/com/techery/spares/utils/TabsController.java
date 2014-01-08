f808633c76234b8294581e30225d2e205d3b1a7a
Sergey Zenchenko <sergeizenchenko@gmail.com>
2014-01-08 14:00:38 +0200

    Replace TabbecActivity wiht TabsController;

--- src/main/java/com/techery/spares/utils/TabsController.java
+++ src/main/java/com/techery/spares/utils/TabsController.java
@@ -1,28 +1,31 @@
-package com.techery.spares.ui.activity;
+package com.techery.spares.utils;
 
-import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 
 import com.techery.spares.adapter.FragmentTabsPagerAdapter;
+import com.techery.spares.ui.activity.BaseActivity;
 
 import java.util.List;
 
-public abstract class TabbedActivity extends BaseActivity implements ActionBar.TabListener {
-    ViewPager viewPager;
+public class TabsController implements ActionBar.TabListener {
+    private ViewPager viewPager;
+    private final BaseActivity activity;
 
-    @Override
-    protected void afterCreateView(Bundle savedInstanceState) {
-        viewPager = findViewPager();
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        setupTabs();
+    public TabsController(BaseActivity activity) {
+        this.activity = activity;
     }
 
-    protected void setupTabs() {
-        final ActionBar actionBar = getSupportActionBar();
+    public void setup(ViewPager viewPager, List<FragmentTabsPagerAdapter.TabItem> tabItems) {
+        this.viewPager = viewPager;
+        activity.getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        setupTabs(this.viewPager, tabItems);
+    }
 
-        FragmentTabsPagerAdapter sectionsPagerAdapter = new FragmentTabsPagerAdapter(getSupportFragmentManager(), buildTabs());
+    protected void setupTabs(ViewPager viewPager, List<FragmentTabsPagerAdapter.TabItem> tabItems) {
+        final ActionBar actionBar = activity.getSupportActionBar();
+        FragmentTabsPagerAdapter sectionsPagerAdapter = new FragmentTabsPagerAdapter(activity.getSupportFragmentManager(),tabItems);
 
         viewPager.setAdapter(sectionsPagerAdapter);
         viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
@@ -43,18 +46,18 @@ public abstract class TabbedActivity extends BaseActivity implements ActionBar.T
         }
     }
 
-    protected abstract List<FragmentTabsPagerAdapter.TabItem> buildTabs();
-
-    protected abstract ViewPager findViewPager();
-
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         viewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
 
     @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
 }
