9cebd34f715ae53b203bc4a5ebdf36412e9e9142
Sergey Zenchenko <sergeizenchenko@gmail.com>
2014-01-08 17:37:46 +0200

    Add support dependencies;

--- src/main/java/com/techery/spares/module/SupportModule.java
+++ src/main/java/com/techery/spares/module/SupportModule.java
@@ -1,7 +1,12 @@
 package com.techery.spares.module;
 
+import android.content.Context;
+
 import com.techery.spares.utils.AnnotationsHelper;
+import com.techery.spares.utils.BinderRetriever;
 import com.techery.spares.utils.ObjectStateHelper;
+import com.techery.spares.utils.intent.IntentBuilder;
+import com.techery.spares.utils.intent.ServiceLauncher;
 
 import javax.inject.Singleton;
 
@@ -20,5 +25,20 @@ public class SupportModule {
     @Singleton
     ObjectStateHelper provideAnnotationsHelper(AnnotationsHelper annotationsHelper) {
         return new ObjectStateHelper(annotationsHelper);
+    }
+
+    @Provides
+    ServiceLauncher provideServiceLauncher(Context context, IntentBuilder intentBuilder) {
+        return new ServiceLauncher(context, intentBuilder);
+    }
+
+    @Provides
+    IntentBuilder provideIntentBuilder(Context context) {
+        return new IntentBuilder(context);
+    }
+
+    @Provides
+    BinderRetriever provideBinderRetriever(Context context) {
+        return new BinderRetriever(context);
     }
 }
